51250023793430ef116f1a2fb5664199dcbf7d44
david.cavalcanti <david.cavalcanti@i.systems.com.br>
2022-11-01 11:25:18 -0300

    Revert "Criando tela de boas-vindas do jogo"
    This reverts commit e67bce87fee3f654b1c7aa232ed07bce2924625b.

--- jogodaadivinhacao.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main (){
-    cout << "**************************************" << endl;
-    cout << "* Bem-vindos ao jogo de adivinhacao! *" << endl;
-    cout << "**************************************" << endl;
-}
