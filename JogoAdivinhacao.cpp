15ccb36d2aa00cee3102dbbd03952a7633854d47
david.cavalcanti <david.cavalcanti@i.systems.com.br>
2022-11-01 11:59:24 -0300

    Adicionando cabeçalho do jogo

---
+++ JogoAdivinhacao.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "****************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhacao *" << endl;
+    cout << "****************************************" << endl;
+}
